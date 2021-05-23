78c6eab413d2c56eba3b9a9773096001d545d83f
fanhorn <84653886+fanhorn@users.noreply.github.com>
2021-05-23 15:29:05 +1000

    Add files via upload

---
+++ BurnableMint.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.6.10;
+
+import "./NewToken.sol";
+import "./ERC20Standart.sol";
+
+contract MintableToken is Ownable, ERC20 {
+    constructor() public ERC20("Gold", "GLD") { }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+}
